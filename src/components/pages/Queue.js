28e07f1247242711fb33fe60630bcb7befd19c63
devinrbopp <devinrbopp@gmail.com>
2022-01-11 14:09:24 -0700

    add to queue working

--- src/components/pages/Queue.js
+++ src/components/pages/Queue.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import Chat from "./Chat"
 import { socket } from "../../App"
+import apiUrl from "../../apiConfig"
 
 export default function Queue(props) {
     const [messagesData, setMessagesData] = useState([])
@@ -13,18 +14,37 @@ export default function Queue(props) {
 		})
     }, [socket])
 
+    const joinQueue = (e) => {
+        e.preventDefault()
+        let preJSONBody = {
+            owner: props.user._id
+        }
+        fetch(apiUrl + '/queue', {
+            method: 'POST',
+            body: JSON.stringify(preJSONBody),
+            headers: { 'Content-Type': 'application/JSON', 'Authorization': 'Bearer ' + props.user.token }
+        })
+            .then(response => response.json())
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {console.log(error)})
+    }
 
     console.log('this is messagesData in queue.js', messagesData)
 
     return(
-        // queue counter will go up here
-        // <QueueDisplay />
+        <>
+            <form onSubmit={joinQueue}>
+                <input type='submit' value='Get In Line!' />
+            </form>
 
-        // chat goes here
-        <Chat 
-            profile={props.profile} 
-            messagesData={messagesData}
-        />
+            <Chat 
+                profile={props.profile} 
+                messagesData={messagesData}
+            />
+        </>
+
 
     )
 }
