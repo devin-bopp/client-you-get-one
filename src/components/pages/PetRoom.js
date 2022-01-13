217262e940d1b8e637e84acf1ec87f4dbbf38a11
devinrbopp <devinrbopp@gmail.com>
2022-01-13 08:57:20 -0700

    player kicked from pet room when their time ends

--- src/components/pages/PetRoom.js
+++ src/components/pages/PetRoom.js
@@ -1,5 +1,18 @@
+import { socket } from "../../App"
+import { useNavigate } from 'react-router-dom'
+import { useEffect } from "react"
 
 export default function PetRoom(props) {
+    const navigate = useNavigate()
+    
+    // boots player from /pet once their time has ended
+    useEffect(() => {
+        socket.on('kick current player', () => {
+            console.log('your time with the pet has ended!')
+            navigate('/queue')
+        })
+    }, [socket])
+
     return (
         <div>
             <img id='pet-img' src='images/pet_500.png' alt='pixel art of a smiling, green frog' />
